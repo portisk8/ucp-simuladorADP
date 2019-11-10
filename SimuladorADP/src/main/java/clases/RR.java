d9f6992d07f49ad6fa0e286ca05b3d2a169b05a4
EliasMachuanin <eliasmachuanin@gmail.com>
2019-11-10 11:51:21 -0300

    RR creeria que funcional

--- SimuladorADP/src/main/java/clases/RR.java
+++ SimuladorADP/src/main/java/clases/RR.java
@@ -1,4 +1,5 @@
 package clases;
+import com.ucp.simuladoradp.main.FifoView;
 import java.util.*;
 import java.util.ArrayList;
 import java.util.logging.Level;
@@ -36,53 +37,67 @@ public class RR extends Algoritmo implements Runnable{
     }
 
     public void run() {
-            Proceso proceso;//creamos un proceso de la clase proceso
+            Proceso proceso, proceso2;//creamos un proceso de la clase proceso
             //this.fifoView.getTimerCpu().setText("0");//seteamos el valor de la vista fifo con valor cero (0)
-            int timer = 0;//creamos una variable timer con inicializacion cero (0)
+            
+            int timer = 0, tiempoDisponible = 0;//creamos una variable timer con inicializacion cero (0)
+            
+            
             while(!super.todosFinalizados()){//mientras haya procesos en la tabla de pendientes por atender
                 for (Proceso procesoActual : super.getProcesos()) {
                     if(procesoActual.getInstante_entrada()==timer){
                         super.getPendiente().addRow(procesoActual);
                     }
                 }
-                if(super.getPendiente().getRowCount()>0){
-                proceso = super.getPendiente().getProceso(super.getPendiente().procesoMenorTiempoEjecucion());//carga proceso de la tabla pendiente a la variable proceso
-                super.getPendiente().removeRow(super.getPendiente().procesoMenorTiempoEjecucion());//una vez cargado el proceso se remueva de la tabla pendiente
-                super.getEjecutando().addRow(proceso);//se envia el proceso a la tabla de procesos en ejecucion
-                for (int i = 0; i < this.getQuanto(); i++) {//para i iniciando en 0 hasta i menor al tiempo de rafaga de cpu, i aumenta en 1
+                if(super.getPendiente().getRowCount()>0 || super.getEjecutando().getRowCount()>0){
+                    if((tiempoDisponible == this.getQuanto())&&(super.getPendiente().getRowCount()>0)&&(super.getEjecutando().getRowCount()>0)){
+                        proceso = super.getPendiente().getProceso(0);//carga proceso de la tabla pendiente a la variable proceso
+                        proceso2 = super.getEjecutando().getProceso(0);
+                        super.getEjecutando().removeRow(0);
+                        super.getPendiente().addRow(proceso2);
+                        super.getPendiente().removeRow(0);//una vez cargado el proceso se remueva de la tabla pendiente
+                        super.getEjecutando().addRow(proceso);//se envia el proceso a la tabla de procesos en ejecucion
+                        tiempoDisponible = 0;
+                    }
+                    else if(super.getPendiente().getRowCount()>0 && super.getEjecutando().getRowCount()==0){
+                        proceso = super.getPendiente().getProceso(0);//carga proceso de la tabla pendiente a la variable proceso
+                        super.getPendiente().removeRow(0);//una vez cargado el proceso se remueva de la tabla pendiente
+                       super.getEjecutando().addRow(proceso);//se envia el proceso a la tabla de procesos en ejecucion
+                       tiempoDisponible = 0;
+                    }
+                    else{
+                       proceso = super.getEjecutando().getProceso(0);
+                    }
                     timer++;//variable timer aumenta en uno
-                    //this.fifoView.getTimerCpu().setText(Integer.toString(timer));//setea el valor del componente de la vist fifo para mostrar en la rafaga de cpu el valor del tiempo
-                    try {
-                        Thread.sleep(1000);//el hilo queda en modo espera por 1 segundo
-                    } catch (InterruptedException ex) {
-                        //Logger.getLogger(FifoView.class.getName()).log(Level.SEVERE, null, ex);//en caso de ocurrir un error en la ejecucion se enviara un mensaje de aviso informando del error
+                    tiempoDisponible++;
+                    super.getEjecutando().getProceso(0).calcularDuracionRestante(1);
+                        //this.fifoView.getTimerCpu().setText(Integer.toString(timer));//setea el valor del componente de la vist fifo para mostrar en la rafaga de cpu el valor del tiempo
+                        try {
+                            Thread.sleep(3000);//el hilo queda en modo espera por 1 segundo
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(FifoView.class.getName()).log(Level.SEVERE, null, ex);//en caso de ocurrir un error en la ejecucion se enviara un mensaje de aviso informando del error
+                        }
+                    if(proceso.getDuracionRestante()<=0){
+                        super.getEjecutando().removeRow(0);//sacamos el proceso de la tabla procesos en ejecucion
+                        super.getListo().addRow(proceso);//cargamos el proceso en la tabla procesos terminados
+                        super.getProcesoByID(proceso.getId()).finalizar();
                     }
-                    if(super.getEjecutando().getProceso(0).getDuracionRestante()<=0){
-                        
-                    }
-                }
-                if(super.getEjecutando().getProceso(0).getDuracionRestante()<=0){
-                    super.getEjecutando().removeRow(0);//sacamos el proceso de la tabla procesos en ejecucion
-                    super.getListo().addRow(proceso);//cargamos el proceso en la tabla procesos terminados
-                    super.getProcesoByID(proceso.getId()).finalizar();
-                }
-                else{
-                    super.getEjecutando().getProceso(0).calcularDuracionRestante(quanto);
-                    super.getPendiente().addRow(proceso);
-                    super.getEjecutando().removeRow(0);
-                    
-                }
             }
                 else{
                     timer++;
+                    //this.fifoView.getTimerCpu().setText(Integer.toString(timer));//setea el valor del componente de la vist fifo para mostrar en la rafaga de cpu el valor del tiempo
                     try {
-                        Thread.sleep(1000);//el hilo queda en modo espera por 1 segundo
+                        Thread.sleep(3000);//el hilo queda en modo espera por 1 segundo
                     } catch (InterruptedException ex) {
-                        //Logger.getLogger(FifoView.class.getName()).log(Level.SEVERE, null, ex);//en caso de ocurrir un error en la ejecucion se enviara un mensaje de aviso informando del error
-                    }
+                        Logger.getLogger(FifoView.class.getName()).log(Level.SEVERE, null, ex);//en caso de ocurrir un error en la ejecucion se enviara un mensaje de aviso informando del error
                 }
             }
+                
+                
+                
         }
+            super.resetProcesos();
+    }
 
     /**
      * @return the quanto
